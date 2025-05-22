72e20c3c2c2c189111c28a91e6e5115ba421169a
LuisLazaroMachado <luislazaro96m@gmail.com>
2025-05-21 20:22:53 -0500

    Primer comit en mi github

---
+++ src/main/java/com/ingsw/practicandoexamenparcial/PracticandoExamenParcialApplication.java
@@ -0,0 +1,13 @@
+package com.ingsw.practicandoexamenparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticandoExamenParcialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticandoExamenParcialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ingsw/practicandoexamenparcial/PracticandoExamenParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsw.practicandoexamenparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticandoExamenParcialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
