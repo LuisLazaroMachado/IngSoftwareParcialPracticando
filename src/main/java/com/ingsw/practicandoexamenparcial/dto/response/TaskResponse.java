d426fd565e1469be6e252bb28e1f5b719af783dc
LuisLazaroMachado <luislazaro96m@gmail.com>
2025-05-22 13:47:57 -0500

    Implementacion de task

---
+++ src/main/java/com/ingsw/practicandoexamenparcial/dto/response/TaskResponse.java
@@ -0,0 +1,13 @@
+package com.ingsw.practicandoexamenparcial.dto.response;
+
+import java.time.LocalDate;
+
+public record TaskResponse(
+        Long id,
+        String title,
+        String description,
+        String status,
+        String developerName,
+        LocalDate startDate,
+        LocalDate endDate
+) {}
---
+++ src/main/java/com/ingsw/practicandoexamenparcial/model/enums/TaskStatus.java
@@ -0,0 +1,8 @@
+package com.ingsw.practicandoexamenparcial.model.enums;
+
+public enum TaskStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
